326cbef3c782aa0c0264cba4b8fc104074adcbc2
Kazuya-S-SI-seminar <39054600+Kazuya-S-SI-seminar@users.noreply.github.com>
2020-07-13 03:16:38 +0900

    Update GroupE_Kadai.java

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -26,7 +26,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        int[] numberList  =  {1,2,3,4,5,6,7, 8, 9 , 10 } ;
+        int[] numberList  =  {1,2,3,4,5,6,7, 8, 9 , 10 };
         
         for ( int i  =  0 ; i  <  numberList.length ; i ++ ) {
             numberList[i]  =  toOdd(numberList[i]) ;
