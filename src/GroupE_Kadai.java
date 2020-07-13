3c84586184a36cf4ea4c647235b11930d571df3c
Yuta-Minami <53511699+Yuta-Minami@users.noreply.github.com>
2020-07-13 13:52:06 +0900

    Update GroupE_Kadai.java

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class Main {
     public static void number_sort(int[] number_array){
         int temp;
-        for(int i = 0;i < number_array.length; i++){
-            for(int j = 0;j < number_array.length-i-1; j++){
-                if(number_array[j] > number_array[j+1]){
+        for(int i = 0; i < number_array.length; i++){
+            for(int j = 0; j < number_array.length - i - 1; j++){
+                if(number_array[j] > number_array[j + 1]){
                     temp = number_array[j];
-                    number_array[j] = number_array[j+1];
-                    number_array[j+1] = temp;
+                    number_array[j] = number_array[j + 1];
+                    number_array[j + 1] = temp;
                 }
             }
         }
@@ -26,7 +26,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        int[] numberList  =  {1, 2, 3, 4, 5, 6 , 7, 8, 9, 10};
+        int[] numberList  =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         
         for (int i = 0; i < numberList.length; i++) {
             numberList[i] = toOdd(numberList[i]);
