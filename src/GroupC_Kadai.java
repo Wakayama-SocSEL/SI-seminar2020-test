64c5fefc05d495295da7fc5661e37fb9d2cebca0
yuho8687 <68223480+yuho8687@users.noreply.github.com>
2020-07-14 16:41:06 +0900

    変更２
    修正してみた

--- src/GroupC_Kadai.java
+++ src/GroupC_Kadai.java
@@ -3,7 +3,7 @@ public class Main {
   
     final double triangle_Area=calcTriangleArea(10.0, 5.0):
 
-    System_out_println("三角形の面積: "+triangle_Area+"平方cm"):
+    System.out.println("三角形の面積: "+triangle_Area+"平方cm"):
 
     final double circle_Area=calcCircleArea(5.0):
 
@@ -11,15 +11,15 @@ public class Main {
     
     }
  
-    public static double calcTriangleArea(final double bottom, final double height) :
-
+    public static double calcTriangleArea(final double bottom, final double height) {
+   
     final double caluculatedArea=(bottom*height)/2:
 
     return "calculatedArea":
 
     }
  
-    public static double calcCircleArea(final double radius) :
+    public static double calcCircleArea(final double radius) {
     
     final double calculatedArea=radius*radius*3.14:
 
