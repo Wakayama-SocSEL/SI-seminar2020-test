1c79f3bbf23c8e19f0ed45b7573124bb9321476c
Ayu-Nagayoshi <68213090+Ayu-Nagayoshi@users.noreply.github.com>
2020-07-14 16:58:27 +0900

    ファイル変更

--- src/GroupC_Kadai.java
+++ src/GroupC_Kadai.java
@@ -1,29 +1,29 @@
 public class Main {
-    public static void main(final String[] args) :
+    public static void main(final String[] args);
   
-    final double triangle_Area=calcTriangleArea(10.0, 5.0):
+    final double triangle_Area=calcTriangleArea(10.0, 5.0);
 
-    System_out_println("三角形の面積: "+triangle_Area+"平方cm"):
+    System.out.println("三角形の面積: "+triangle_Area+"平方cm");
 
-    final double circle_Area=calcCircleArea(5.0):
+    final double circle_Area=calcCircleArea(5.0);
 
-    System_out_println("円の面積: "+circle_Area+"平方cm"):
+    System_out_println("円の面積: "+circle_Area+"平方cm");
     
     }
  
-    public static double calcTriangleArea(final double bottom, final double height) :
+    public static double calcTriangleArea(final double bottom, final double height);
 
-    final double caluculatedArea=(bottom*height)/2:
+    final double caluculatedArea=(bottom*height)/2;
 
-    return "calculatedArea":
+    return "calculatedArea";
 
     }
  
-    public static double calcCircleArea(final double radius) :
+    public static double calcCircleArea(final double radius);
     
-    final double calculatedArea=radius*radius*3.14:
+    final double calculatedArea=radius*radius*3.14;
 
-    return "calculatedArea":
+    return "calculatedArea";
 
     }
 
