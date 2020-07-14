522b21495e3b562aba4763ad48e9cbb748b76015
Mizuki-Inui <67994542+Mizuki-Inui@users.noreply.github.com>
2020-07-14 18:23:21 +0900

    Update GroupE_Kadai.java

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class Main {
-    public static void number_sort(int[] number_array){
+    public static void number_sort(int[] number_array) {
         int temp;
-        for(int i = 0; i < number_array.length; i++){
-            for(int j = 0; j < number_array.length - i - 1; j++){
-                if(number_array[j] > number_array[j + 1]){
+        for(int i = 0; i < number_array.length; i++) {
+            for(int j = 0; j < number_array.length - i - 1; j++) {
+                if(number_array[j] > number_array[j + 1]) {
                     temp = number_array[j];
                     number_array[j] = number_array[j + 1];
                     number_array[j + 1] = temp;
@@ -13,15 +13,11 @@ public class Main {
         }
     }
     
-    private static int toOdd(int Value)
-    {
+    private static int toOdd(int Value) {
         int doubleValue = Value * 2;
-        if (Value % 2 == 0)
-        {
+        if (Value % 2 == 0) {
             return Value;
-        }
-        else
-        {
+        } else {
             return doubleValue;
         }
     }
