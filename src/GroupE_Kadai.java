97ff4ca61b3260c14649aec23115923e5b10e8f9
Fukumoto-Kotaro <60866255+Fukumoto-Kotaro@users.noreply.github.com>
2020-07-17 17:11:07 +0900

    Update GroupE_Kadai.java

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -12,6 +12,7 @@ public class Main {
             }
         }
     }
+    //test
     
     private static int toOdd(int Value)
     {
@@ -26,7 +27,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        int[] numberList  =  {1, 2, 3, 4, 5 , 6 , 7, 8, 9 , 10 } ;
+        int[] numberList  =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ;
         
         for ( int i = 0; i < numberList.length ; i++) {
             numberList[i]  =  toOdd(numberList[i]) ;
