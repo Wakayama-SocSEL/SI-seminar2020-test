59b97abf70284486a25e8601c92cd355e9efaf23
Fukumoto-Kotaro <60866255+Fukumoto-Kotaro@users.noreply.github.com>
2020-07-19 14:59:40 +0900

    Update GroupE_Kadai.java

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -2,21 +2,23 @@ import java.util.*;
 public class Main {
     public static void number_sort(int[] number_array){
         int temp;
-        for(int i　=　0;i<number_array.length;i++){
-            for(int j=0;j<number_array.length-i-1;j++){
-                if(number_array[j]>number_array[j+1]){
+        for(int i=0;i<number_array.length;i++){
+            for(it j=0; j < number_array.length-i-1; j++){
+                if(number_array[j] > number_array[j+1]){
                     temp=number_array[j];
-                    number_array[j]=number_array[j+1];
-                    number_array[j+1]=temp;
+                    number_array[j] = number_array[j+1];
+                    number_array[j+1] = temp;
                 }
             }
         }
     }
+    //test;
+    //test2
     
     private static int toOdd(int Value)
     {
-        int doubleValue =Value *2;
-        if (Value %2 ==0)
+        int doubleValue = Value * 2;
+        if (Value %2 == 0)
         {
             return Value;
         }
@@ -26,14 +28,14 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        int[] numberList  =  {1,2,3, 4,5 ,6 ,7, 8, 9 , 10 } ;
+        int[] numberList  =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ;
         
-        for ( int i  =  0 ; i  <  numberList.length ; i ++ ) {
+        for ( int i = 0; i < numberList.length ; i++) {
             numberList[i]  =  toOdd(numberList[i]) ;
         }
         
         number_sort(numberList);
-        for ( int i  =  0 ; i  <  numberList.length ; i ++ ) {
+        for ( int i = 0 ; i < numberList.length ; i++)  {
             System.out.println(numberList[i]) ;
         }
     }
