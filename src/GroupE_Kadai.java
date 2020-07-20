207704cacaa32c34c4a5ee7a3c33939aad8aca8b
mochyr <68263535+mochyr@users.noreply.github.com>
2020-07-20 16:19:47 +0900

    Update GroupE_Kadai.java

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class Main {
-    public static void number_sort (int[] number_array) {
+    public static void number_sort (int [] number_array) {
         int temp;
         for (int i = 0; i < number_array.length; i++) {
             for (int j = 0; j < number_array.length - i - 1; j++) {
-                if (number_array[j] > number_array[j + 1 ]) {
+                if (number_array[j] > number_array[j + 1]) {
                     temp = number_array[j];
                     number_array[j] = number_array[j + 1];
                     number_array[j + 1] = temp;
