87ed50e04e6ac0553eaae46bc42327ca7453fd5e
takaoka <46779101+TakaokaRyoga@users.noreply.github.com>
2020-07-20 17:00:02 +0900

    Update GroupE_Kadai.java
    Method間の改行を追加

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -30,6 +30,7 @@ public class Main
             return doubleValue;
         }
     }
+
     public static void main(String[] args)
     {
         int[] numberList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
