1b0f292e415e40bedca819da2d0d00d4c250ed50
takaoka <46779101+TakaokaRyoga@users.noreply.github.com>
2020-07-20 17:02:40 +0900

    Update GroupE_Kadai.java
    ちゃんと検出するかてすと

--- src/GroupE_Kadai.java
+++ src/GroupE_Kadai.java
@@ -30,7 +30,6 @@ public class Main
             return doubleValue;
         }
     }
-
     public static void main(String[] args)
     {
         int[] numberList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
